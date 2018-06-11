3a3525e47a81dac16ee57477bdf1ad3b22cf4dc3
troyibm <tdugger@us.ibm.com>
2018-06-11 16:08:48 -0500

    adds --minimize and --maximize cli options

--- file-explorer/js/app.js
+++ file-explorer/js/app.js
@@ -1,3 +1,7 @@
+const { DirService } = require("./js/Service/Dir");
+const argv = require("minimist")(nw.App.argv),
+    dirService = new DirService(argv._[0]);
+
 const { I18nService } = require("./js/Service/I18n"),
     { LangSelectorView } = require("./js/View/LangSelector"),
     { dictionary } = require("./js/Data/dictionary"),
@@ -7,22 +11,26 @@ new LangSelectorView(document.querySelector("[data-bind=langSelector]"), i18nSer
 const { TitleBarActionsView } = require("./js/View/TitleBarActions");
 new TitleBarActionsView(document.querySelector("[data-bind=titlebar]"), i18nService);
 
-const { DirService } = require("./js/Service/Dir");
 const { DirListView } = require("./js/View/DirList");
 const { FileListView } = require("./js/View/FileList");
 const { TitleBarPathView } = require("./js/View/TitleBarPath");
-dirService = new DirService();
+const { ContextMenuView } = require("./js/View/ContextMenu");
+const { FileService } = require("./js/Service/File"),
+    fileService = new FileService(dirService);
 
 new DirListView(document.querySelector("[data-bind=dirList]"), dirService);
 new TitleBarPathView(document.querySelector("[data-bind=path]"), dirService);
-dirService.notify();
-
-const { FileService } = require("./js/Service/File"),
-    { ContextMenuView } = require("./js/View/ContextMenu"),
-    fileService = new FileService(dirService);
-
 new FileListView(document.querySelector("[data-bind=fileList]"), dirService, i18nService, fileService);
 new ContextMenuView(fileService, i18nService);
 
 const { TrayView } = require("./js/View/Tray");
 new TrayView("");
+
+if (argv.minimize) {
+    nw.Window.get().minimize();
+}
+if (argv.maximize) {
+    nw.Window.get().maximize();
+}
+
+dirService.notify();
