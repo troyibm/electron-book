d889390cb26316df8044760f2675715bfd85ef7f
troyibm <tdugger@us.ibm.com>
2018-06-28 21:23:28 -0500

    adds toolbar  button to maximize and restore window

--- static-proto/app/js/Components/Header.jsx
+++ static-proto/app/js/Components/Header.jsx
@@ -1,16 +1,55 @@
 import React from "react";
+import { remote } from "electron";
+const win = remote.getCurrentWindow();
 
 export default class Header extends React.Component {
 
+    constructor(props) {
+        super(props);
+        this.state = { isMaximized: win.isMaximized() };
+    }
+
+    componentWillMount() {
+        win.on("maximize", this.updateState);
+        win.on("unmaximize", this.updateState);
+    }
+    
+    updateState = () => {
+        this.setState({isMaximized: win.isMaximized()});
+    }
+
     render() {
+        const { isMaximized } = this.state;
         return (
             <header className="toolbar toolbar-header">
                 <div className="toolbar-actions">
-                    <button className="btn btn-default pull-right">
+                    <button className="btn btn-default pull-right" onClick={this.onClose}>
                         <span className="icon icon-cancel"></span>
                     </button>
+                    { isMaximized ? (
+                        <button className="btn btn-default pull-right" onClick={this.onRestore}>
+                            <span className="icon icon-resize-small"></span>
+                        </button>
+                        ): (
+                        <button className="btn btn-default pull-right" onClick={this.onMaximize}>
+                            <span className="icon icon-resize-full"></span>
+                        </button>
+                        )
+                    }
                 </div>
             </header>
         );
+    }
+
+    onRestore = () => {
+        win.restore();
+    }
+
+    onMaximize = () => {
+        win.maximize();
+    }
+
+    onClose = () => {
+        win.close();
     }
 }
