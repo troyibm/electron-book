0cf3e3d4d47f30d538fe11fbcbdca7295d35aa44
troyibm <tdugger@us.ibm.com>
2018-07-07 16:13:10 -0500

    fixes webpack build problem so that name prompt works now

--- static-proto/webpack.config.js
+++ static-proto/webpack.config.js
@@ -17,7 +17,7 @@ module.exports = {
                     loader: "babel-loader",
                     options: {
                         presets: [ "es2017", "react" ],
-                        plugins: [ "transform-class-properties" ]
+                        plugins: [ "transform-class-properties", "transform-object-rest-spread" ]
                     }
                 }]
             },
